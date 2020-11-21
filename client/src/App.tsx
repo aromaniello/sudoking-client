f4db9b39a9fdb8cb8bb260a33fe9a25d63c1a019
Agustin Romaniello <agustin@romaniello.com.ar>
2020-11-21 20:38:04 -0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Sudoking
+    </div>
+  );
+}
+
+export default App;
