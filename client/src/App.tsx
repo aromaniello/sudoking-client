e8356b918e8ad92ae4e34856af89916e664a7bf3
Agustin Romaniello <agustin@romaniello.com.ar>
2020-11-26 19:51:27 -0300

    add real sample board

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,9 +12,30 @@ function App() {
       return range(9).map((column) => null);
     });
 
-    board[1][2] = 5;
-    board[6][3] = 9;
-    board[5][7] = 2;
+    board[0][4] = 5;
+    board[0][8] = 8;
+    board[1][2] = 4;
+    board[1][3] = 9;
+    board[1][6] = 5;
+    board[2][0] = 1;
+    board[2][4] = 4;
+    board[2][6] = 9;
+    board[3][3] = 8;
+    board[3][5] = 1;
+    board[3][8] = 2;
+    board[4][4] = 9;
+    board[5][6] = 4;
+    board[5][7] = 6;
+    board[6][1] = 2;
+    board[6][7] = 9;
+    board[7][0] = 8;
+    board[7][1] = 1;
+    board[7][4] = 7;
+    board[8][0] = 7;
+    board[8][1] = 6;
+    board[8][3] = 4;
+    board[8][5] = 5;
+    board[8][7] = 1;
 
     return board;
   }
